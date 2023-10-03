29d8ac34ad393bb9a8f36ac21a33d2f30866561e
Kirpal Grewal <kirpal@outlook.com>
2023-10-03 21:52:49 +0100

    partial Cholesky

--- gauss-lib/src/lib.rs
+++ gauss-lib/src/lib.rs
@@ -5,13 +5,11 @@
     clippy::complexity,
     clippy::style
 )]
-use std::{
-    cmp::{Ordering, Reverse},
-    collections::BinaryHeap,
-};
+use std::{cmp::Reverse, collections::BinaryHeap};
 
 use faer::{solvers::Solver, Faer, IntoFaer, IntoNalgebra, Mat};
 use nalgebra::{Dyn, Matrix, VecStorage};
+use ordered_float::NotNan;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone)]
@@ -65,12 +63,14 @@ impl<T> GaussProcs<T> {
     pub fn interpolate(&self, x2: &[T]) -> Result<(Mat<f64>, Mat<f64>), ProcessError> {
         let x1 = &self.process.inputs;
         let y1 = &self.process.res;
-        let autocorr: Mat<f64> = self
-            .process
-            .autocorr
-            .view_range(.., ..)
-            .into_faer()
-            .to_owned();
+        let n = x1.len();
+        // let autocorr: Mat<f64> = self
+        //     .process
+        //     .autocorr
+        //     .view_range(.., ..)
+        //     .into_faer()
+        //     .to_owned();
+        let autocorr = Mat::from_fn(n, n, |i, j| (self.metric)(&x1[i], &x1[j]));
         let crosscorr = Mat::from_fn(x1.len(), x2.len(), |i, j| (self.metric)(&x1[i], &x2[j]));
         let postcorr = Mat::from_fn(x2.len(), x2.len(), |i, j| (self.metric)(&x2[i], &x2[j]));
         let y1 = Mat::from_fn(y1.len(), 1, |i, _| y1[i]);
@@ -85,46 +85,114 @@ impl<T> GaussProcs<T> {
         Ok((mu, sigma))
     }
 
-    // restrict the matrix to invert to be the n (TBD) closest values to the desired value
-    // limitation of only infering one value at a time
-    pub fn smart_interpolate(&self, x2: &T) -> Result<(Mat<f64>, Mat<f64>), ProcessError> {
-        // make more effecient with custom binary heap
-        // as f64 does not implement Ord
-        // self.process
-        //     .inputs
-        //     .iter()
-        //     .map(|(val)| (self.metric)(val, x2))
-        //     .enumerate()
-        //     .sort();
-        // let x1 = &self.process.inputs;
-        // let y1 = &self.process.res;
+    pub fn interpolate_one(&self, x2: &T) -> Result<(Mat<f64>, Mat<f64>), ProcessError> {
+        let n = &self.process.inputs.len();
+        let mut heap = BinaryHeap::with_capacity(n + 1);
+
+        self.process
+            .inputs
+            .iter()
+            .map(|val| NotNan::new((self.metric)(val, x2)).expect("NaN from metric"))
+            .enumerate()
+            .for_each(|(i, num)| {
+                heap.push(Reverse((num, i)));
+            });
+
+        let indices: Vec<usize> = heap.into_vec().iter().map(|&Reverse((_, i))| i).collect();
+        let x1: Vec<&T> = indices.iter().map(|i| &self.process.inputs[*i]).collect();
+        let y1: Vec<&f64> = indices.iter().map(|i| &self.process.res[*i]).collect();
+        let n = x1.len();
         // let autocorr: Mat<f64> = self
         //     .process
         //     .autocorr
         //     .view_range(.., ..)
         //     .into_faer()
         //     .to_owned();
-        // let crosscorr = Mat::from_fn(x1.len(), x2.len(), |i, j| (self.metric)(&x1[i], &x2[j]));
+        let autocorr = Mat::from_fn(n, n, |i, j| (self.metric)(&x1[i], &x1[j]));
+        let crosscorr = Mat::from_fn(n, 1, |i, _| (self.metric)(&x1[i], &x2));
+        let postcorr = Mat::from_fn(1, 1, |_, _| (self.metric)(&x2, &x2));
+        let y1 = Mat::from_fn(n, 1, |i, _| *y1[i]);
+        // println!("{:?}", autocorr);
+        let chol_res = match autocorr.cholesky(faer::Side::Lower) {
+            Ok(value) => value.solve(&crosscorr),
+            Err(_) => return Err(ProcessError::CholeskyFaiure),
+        };
 
-        let n = 1000;
+        let mu = { chol_res.transpose() * &y1 };
+        let sigma = { postcorr - chol_res.transpose() * crosscorr };
+        Ok((mu, sigma))
+    }
 
-        let mut metric: Vec<(usize, f64)> = self
-            .process
+    // restrict the matrix to invert to be the n (TBD) closest values to the desired value
+    // limitation of only infering one value at a time
+    pub fn smart_interpolate(&self, x2: &T) -> Result<(Mat<f64>, Mat<f64>), ProcessError> {
+        let n = 6561;
+
+        let mut heap = BinaryHeap::with_capacity(n + 1);
+
+        self.process
             .inputs
             .iter()
-            .map(|(val)| (self.metric)(val, x2))
+            .map(|val| NotNan::new((self.metric)(val, x2)).expect("NaN from metric"))
             .enumerate()
-            .collect();
+            .for_each(|(i, num)| {
+                heap.push(Reverse((num, i)));
+                if heap.len() > n {
+                    heap.pop();
+                }
+            });
 
-        metric.sort_by(|&a, &b| a.1.partial_cmp(&b.1).unwrap_or(Ordering::Equal));
-        let indices: Vec<usize> = metric.into_iter().take(n).map(|(id, _)| id).collect();
+        let indices: Vec<usize> = heap.into_vec().iter().map(|&Reverse((_, i))| i).collect();
 
+        println!("sorted");
         let x1: Vec<&T> = indices.iter().map(|i| &self.process.inputs[*i]).collect();
         let y1: Vec<&f64> = indices.iter().map(|i| &self.process.res[*i]).collect();
 
         let autocorr = Mat::from_fn(n, n, |i, j| (self.metric)(&x1[i], &x1[j]));
-        let crosscorr = Mat::from_fn(n, 1, |i, j| (self.metric)(&x1[i], &x2));
-        let postcorr = Mat::from_fn(1, 1, |i, j| (self.metric)(&x2, &x2));
+        let crosscorr = Mat::from_fn(n, 1, |i, _| (self.metric)(&x1[i], &x2));
+        let postcorr = Mat::from_fn(1, 1, |_, _| (self.metric)(&x2, &x2));
+        let y1 = Mat::from_fn(n, 1, |i, _| *y1[i]);
+
+        let chol_res = match autocorr.cholesky(faer::Side::Lower) {
+            Ok(value) => value.solve(&crosscorr),
+            Err(_) => return Err(ProcessError::CholeskyFaiure),
+        };
+
+        let mu = { chol_res.transpose() * &y1 };
+        let sigma = { postcorr - chol_res.transpose() * crosscorr };
+        Ok((mu, sigma))
+    }
+
+    pub fn dyn_smart_interpolate(
+        &self,
+        x2: &T,
+        n: usize,
+    ) -> Result<(Mat<f64>, Mat<f64>), ProcessError> {
+        // let n = 6561;
+
+        let mut heap = BinaryHeap::with_capacity(n + 1);
+
+        self.process
+            .inputs
+            .iter()
+            .map(|val| NotNan::new((self.metric)(val, x2)).expect("NaN from metric"))
+            .enumerate()
+            .for_each(|(i, num)| {
+                heap.push(Reverse((num, i)));
+                if heap.len() > n {
+                    heap.pop();
+                }
+            });
+
+        let indices: Vec<usize> = heap.into_vec().iter().map(|&Reverse((_, i))| i).collect();
+
+        println!("sorted");
+        let x1: Vec<&T> = indices.iter().map(|i| &self.process.inputs[*i]).collect();
+        let y1: Vec<&f64> = indices.iter().map(|i| &self.process.res[*i]).collect();
+
+        let autocorr = Mat::from_fn(n, n, |i, j| (self.metric)(&x1[i], &x1[j]));
+        let crosscorr = Mat::from_fn(n, 1, |i, _| (self.metric)(&x1[i], &x2));
+        let postcorr = Mat::from_fn(1, 1, |_, _| (self.metric)(&x2, &x2));
         let y1 = Mat::from_fn(n, 1, |i, _| *y1[i]);
 
         let chol_res = match autocorr.cholesky(faer::Side::Lower) {
@@ -138,48 +206,57 @@ impl<T> GaussProcs<T> {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use std::{
-        cmp::{Ordering, Reverse},
-        collections::BinaryHeap,
-    };
+// #[cfg(test)]
+// mod tests {
+//     use std::{
+//         cmp::{Ordering, Reverse},
+//         collections::BinaryHeap,
+//     };
 
-    use assert_approx_eq::*;
-    use faer::{mat, solvers::Solver, Faer, Mat};
-    // use super::*;
-    // use serde_json::Result;
-    #[test]
-    fn it_works() {
-        // let m1 = Mat::from_fn(3, 1, |i, j| i as f64 + j as f64);
-        // let a = mat![[4., 12., -16.], [12., 37., -43.], [-16., -43., 98f64],];
-        // let decomp = a.cholesky(faer::Side::Lower).unwrap();
-        // let sol = decomp.solve(&m1);
-        // let round = a * sol;
-        // for i in 0..3 {
-        //     assert_approx_eq!(m1.get(i, 0), round.get(i, 0))
-        // }
-        // assert!(1 == 2);
-        let mut list = vec![5., 6., 10., 4., 7., 1., 19.];
-        list.sort_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Equal));
-        list.iter();
-        println!("{:?}", list);
-        panic!()
-        // let n = 3; // Replace 3 with any number you want
-        // let mut heap = BinaryHeap::new();
+//     use assert_approx_eq::*;
+//     use faer::{mat, solvers::Solver, Faer, Mat};
+//     use ordered_float::NotNan;
+//     // use super::*;
+//     // use serde_json::Result;
+//     #[test]
+//     fn it_works() {
+//         // let m1 = Mat::from_fn(3, 1, |i, j| i as f64 + j as f64);
+//         // let a = mat![[4., 12., -16.], [12., 37., -43.], [-16., -43., 98f64],];
+//         // let decomp = a.cholesky(faer::Side::Lower).unwrap();
+//         // let sol = decomp.solve(&m1);
+//         // let round = a * sol;
+//         // for i in 0..3 {
+//         //     assert_approx_eq!(m1.get(i, 0), round.get(i, 0))
+//         // }
+//         // assert!(1 == 2);
+//         let mut list = vec![
+//             NotNan::new(5.).unwrap(),
+//             NotNan::new(6.).unwrap(),
+//             NotNan::new(2.).unwrap(),
+//             NotNan::new(511.).unwrap(),
+//             NotNan::new(23.).unwrap(),
+//             NotNan::new(1.).unwrap(),
+//             NotNan::new(8.).unwrap(),
+//         ];
+//         // list.sort_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Equal));
+//         // list.iter();
+//         // println!("{:?}", list);
+//         // panic!()
+//         let n = 3; // Replace 3 with any number you want
+//         let mut heap = BinaryHeap::new();
 
-        // list.iter().enumerate().for_each(|(i, &num)| {
-        //     heap.push(Reverse((num, i)));
-        //     if heap.len() > n {
-        //         heap.pop();
-        //     }
-        // });
+//         list.iter().enumerate().for_each(|(i, &num)| {
+//             heap.push(Reverse((num, i)));
+//             if heap.len() > n {
+//                 heap.pop();
+//             }
+//         });
 
-        // let mut largest = heap.into_vec();`
-        // largest.sort();
+//         let mut largest = heap.into_vec();
+//         largest.sort();
 
-        // let indices_of_largest: Vec<usize> = largest.iter().map(|&Reverse((_, i))| i).collect();
-        // println!("{:?}", indices_of_largest);
-        // panic!()
-    }
-}
+//         let indices_of_largest: Vec<usize> = largest.iter().map(|&Reverse((_, i))| i).collect();
+//         println!("{:?}", indices_of_largest);
+//         panic!()
+//     }
+// }
--- gauss-procs/src/main.rs
+++ gauss-procs/src/main.rs
@@ -1,5 +1,6 @@
 use gauss_lib::*;
 use itertools::Itertools;
+use std::time::Instant;
 // test function from sfu test functions
 // lim et al nonpolynomial function
 fn lim_nonpoly(x: &(f64, f64)) -> f64 {
@@ -21,8 +22,31 @@ fn main() {
     // println!("{:?}", outputs);
     println!("{:?}, {:?}", &inputs[100], &inputs[99]);
     println!("{}", metric(&inputs[100], &inputs[99]));
+    println!("{}", inputs.len());
     let proc = GaussProcs::new(inputs, outputs, 0., metric).unwrap();
-    // println!("{:?}", proc.interpolate(&[(0.215, 0.255)]).unwrap());
+
+    let now = Instant::now();
     println!("{:?}", proc.smart_interpolate(&(0.215, 0.255)).unwrap());
+    let elapsed = now.elapsed();
+    println!("Elapsed, trunc: {:.2?}", elapsed);
+
+    let now = Instant::now();
+    println!(
+        "{:?}",
+        proc.dyn_smart_interpolate(&(0.215, 0.255), 6561).unwrap()
+    );
+    let elapsed = now.elapsed();
+    println!("Elapsed, trunc: {:.2?}", elapsed);
+
+    let now = Instant::now();
+    println!("{:?}", proc.interpolate(&[(0.215, 0.255)]).unwrap());
+    let elapsed = now.elapsed();
+    println!("Elapsed, non trunc: {:.2?}", elapsed);
+
+    let now = Instant::now();
+    println!("{:?}", proc.interpolate_one(&(0.215, 0.255)).unwrap());
+    let elapsed = now.elapsed();
+    println!("Elapsed, trunc: {:.2?}", elapsed);
+
     println!("{:?}", lim_nonpoly(&(0.215, 0.255)));
 }
